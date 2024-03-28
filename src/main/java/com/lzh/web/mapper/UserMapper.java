0f05d7ad2316d2582ce59d4d94f62e38f5fd7bcf
xx <937815432@qq,com>
2024-03-28 19:16:57 +0800

    add: User相关Mapper以及Service

---
+++ src/main/java/com/lzh/web/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.lzh.web.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lzh.web.model.entity.User;
+
+/**
+ * 数据库操作类
+ * @author lzh
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
