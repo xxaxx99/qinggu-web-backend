e560f4242d906ab61f745d75eb59b0050641e2ba
xx <937815432@qq,com>
2024-03-28 19:28:16 +0800

    add: 自定义全局异常处理器

---
+++ src/main/java/com/lzh/web/exception/BusinessException.java
@@ -0,0 +1,34 @@
+package com.lzh.web.exception;
+
+import com.lzh.web.common.ErrorCode;
+
+/**
+ * 自定义异常类
+ * @author lzh
+ */
+public class BusinessException extends RuntimeException {
+
+    /**
+     * 错误码
+     */
+    private final int code;
+
+    public BusinessException(int code, String message) {
+        super(message);
+        this.code = code;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.code = errorCode.getCode();
+    }
+
+    public BusinessException(ErrorCode errorCode, String message) {
+        super(message);
+        this.code = errorCode.getCode();
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
